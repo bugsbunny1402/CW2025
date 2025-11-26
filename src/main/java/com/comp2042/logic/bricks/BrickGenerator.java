72d05682931e6a1077c9adf4038cd27b894e75e4
Sanjanaah <sanjanaahz@gmail.com>
2025-11-26 16:25:52 +0800

    maintenance : document RandomBrickGenerator and BrickGenerator,add safety checks - Added Javadoc explaining random brick selection and queue behaviour - Added comments to key fields for maintainability - Added defensive check in getNextBrick() - No functional behaviour changed

--- src/main/java/com/comp2042/logic/bricks/BrickGenerator.java
+++ src/main/java/com/comp2042/logic/bricks/BrickGenerator.java
@@ -1,5 +1,22 @@
 package com.comp2042.logic.bricks;
 
+/**
+ * BrickGenerator
+ *
+ * This interface defines how bricks are created for the Tetris game.
+ *
+ * Responsibilities:
+ * - Provide the current brick that should be dropped onto the board.
+ * - Provide the next brick (used for preview or internal queueing).
+ *
+ * Implementations:
+ * - RandomBrickGenerator: selects bricks randomly.
+ *  *
+ *  * This generator is used by SimpleBoard to create new bricks whenever
+ *  * the previous brick locks into place.
+ *
+ */
+
 public interface BrickGenerator {
 
     Brick getBrick();
